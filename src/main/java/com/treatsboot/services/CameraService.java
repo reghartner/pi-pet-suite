72ca54f13be4007e4ff24557fd48f6cf0f5253c9
Chuck Costa <ccosta@vistaprint.com>
2019-12-12 17:40:15 -0500

    try to speed up pictures

--- src/main/java/com/treatsboot/services/CameraService.java
+++ src/main/java/com/treatsboot/services/CameraService.java
@@ -11,6 +11,8 @@ import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static uk.co.caprica.picam.CameraConfiguration.cameraConfiguration;
 
@@ -39,7 +41,7 @@ public class CameraService
     public byte[] getGif() throws Exception
     {
         int msBetweenFrames = 500;
-        int numFrames = 50;
+        int numFrames = 25;
 
         CameraConfiguration config = cameraConfiguration()
             .width(600)
@@ -62,10 +64,19 @@ public class CameraService
             // between frames, which loops continuously
             GifSequenceWriter writer = new GifSequenceWriter(imageOutputStream, 5, msBetweenFrames, true);
 
+            List<byte[]> imageBytesList = new ArrayList<>();
+
             for (int i = 0; i < numFrames; i++)
             {
                 camera.takePicture(handler);
-                InputStream in = new ByteArrayInputStream(handler.result());
+                imageBytesList.add(handler.result());
+            }
+
+            System.out.println("Photos captured, generating GIF...");
+
+            for(int i = 0; i < imageBytesList.size(); i++)
+            {
+                InputStream in = new ByteArrayInputStream(imageBytesList.get(i));
                 BufferedImage bImageFromConvert = ImageIO.read(in);
                 writer.writeToSequence(bImageFromConvert);
             }
